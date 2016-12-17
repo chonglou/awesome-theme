d86599e703f3e4c906895db766e98fe4da6de1df
flamen <jitang.zheng@gmail.com>
2016-12-17 15:10:46 -0800

    install packages

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+sudo pacman -S grub vim zsh
+sudo pacman -S iw net-tools screen vim base-devel wpa_supplicant dialog screen tmux --needed 
+sudo pacman -S xorg fcitx-im fcitx fcitx-googlepinyin
+sudo pacman -S awesome xdm xdm-archlinux
+sudo pacman -S terminus-font wqy-bitmapfont wqy-microhei wqy-zenhei ttf-arphic-uming ttf-arphic-ukai adobe-source-han-sans-cn-fonts adobe-source-han-sans-tw-fonts opendesktop-fonts
+sudo pacman -S lilyterm rxvt-unicode leafpad firefox firefox-i18n-zh-cn chromium  smplayer
