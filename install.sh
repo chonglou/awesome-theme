070aed864880341f5cb69ecb23ca1036d755f706
Jeremy Zheng <jitang.zheng@gmail.com>
2017-11-06 04:29:22 -0800

    change packages

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-sudo pacman -S grub vim zsh
-sudo pacman -S iw net-tools screen vim base-devel wpa_supplicant dialog screen tmux --needed 
-sudo pacman -S xorg fcitx-im fcitx fcitx-googlepinyin
-sudo pacman -S awesome xdm xdm-archlinux
-sudo pacman -S terminus-font wqy-bitmapfont wqy-microhei wqy-zenhei ttf-arphic-uming ttf-arphic-ukai adobe-source-han-sans-cn-fonts adobe-source-han-sans-tw-fonts opendesktop-fonts
-sudo pacman -S lilyterm rxvt-unicode leafpad firefox firefox-i18n-zh-cn chromium  smplayer
+
+sudo pacman -S vim zsh sudo
+sudo pacman -S iw net-tools screen base-devel wpa_supplicant dialog screen tmux 
+sudo pacman -S xorg-apps xorg xorg-drivers --needed
+sudo pacman -S fcitx-im fcitx-googlepinyin 
+sudo pacman -S awesome vicious dex rlwrap xdm xdm-archlinux terminus-font 
+sudo pacman -S wqy-bitmapfont wqy-microhei wqy-zenhei ttf-arphic-uming ttf-arphic-ukai adobe-source-han-sans-cn-fonts adobe-source-han-sans-tw-fonts opendesktop-fonts 
+sudo pacman -S rxvt-unicode leafpad firefox firefox-i18n-zh-cn chromium  smplayer smplayer-themes smplayer-skins
+sudo pacman -S texlive-most texlive-lang graphviz calibre atom poppler-data xpdf
